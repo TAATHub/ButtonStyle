4b3c033ef636613d3a6b2aa173fec8ab66ae7733
TAAT <20purpleblue10@gmail.com>
2023-01-12 22:18:20 +0900

    Initial Commit

---
+++ ButtonStyle/ButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStyleApp.swift
+//  ButtonStyle
+//
+//  Created by 董 亜飛 on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
